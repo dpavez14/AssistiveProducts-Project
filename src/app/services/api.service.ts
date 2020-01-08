a0ab3bec87cb04b8a5cbe0b5a8d7ff24505e6e16
Daniel Pavez <daniel.pavez@haulmer.com>
2020-01-08 06:08:50 +0100

    feat(comments): Comments dialog

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Team} from "../models/team";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Team} from '../models/team';
 
 @Injectable({
   providedIn: 'root'
